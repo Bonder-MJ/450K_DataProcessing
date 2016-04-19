7d9a23a0718ad2d8e3f4d6301b13253876baa82e
Marc Jan Bonder <bonder.m.j@gmail.com>
2016-04-19 14:23:37 +0200

    Updated install list

--- whatToInstall.R
+++ whatToInstall.R
@@ -1,10 +1,13 @@
 source("http://bioconductor.org/biocLite.R")
 biocLite()
 
+##Minimal pipeline (only works on R 2 & R 3)
 biocLite("lumi")
 biocLite("methylumi")
-install.packages("RPMM")
+
+##Extended pipeline (only works on R 2)
 biocLite("preprocessCore")
 biocLite("minfi")
 biocLite("matrixStats")
 biocLite("IlluminaHumanMethylation450k.db")
+install.packages("RPMM")
