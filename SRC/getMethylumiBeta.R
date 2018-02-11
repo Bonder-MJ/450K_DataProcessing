e36aa3e575c78253ef790cf296e5d8c931295d6d
Marc Jan Bonder <bonder.m.j@gmail.com>
2018-02-11 17:01:32 +0000

    Minor updates

--- SRC/getMethylumiBeta.R
+++ SRC/getMethylumiBeta.R
@@ -69,14 +69,22 @@ getBetaMj <- function(u, m, alfa=100){
   #check and "correct" for negative values
   indexNegU <- which(u < 0, arr.ind=TRUE)
   indexNegM <- which(m < 0, arr.ind=TRUE)
-  u[indexNegU] <- 0
-  m[indexNegM] <- 0
+  
+  if(length(indexNegU)>0 || length(indexNegM)>0){
+    cat("\tWarning: negative values introduced. Value is replaced by 0.\n")
+    
+    u[indexNegU] <- 0
+    m[indexNegM] <- 0
+  }
+  
   
   #check and "correct" for non-numeric values
   
   indexNegU <- which(!is.numeric(u), arr.ind=TRUE)
   indexNegM <- which(!is.numeric(m), arr.ind=TRUE)
   if(length(indexNegU)>0 || length(indexNegM)>0){
+    cat("\tWarning: non-numeric values introduced. Value is replaced by 0.\n")
+    
     u[indexNegU] <- 0
     m[indexNegM] <- 0
   }
